3fb64465b51203377c2c9a37ebd2464644cffa5a
Devarsh shukla <Devarsh14@gmail.com>
2023-09-08 19:14:15 +1000

    Js basics of datatypes

---
+++ 20JavasScriptProjects/01_basics/03_conversionAndOperations.js
@@ -0,0 +1,14 @@
+let age = 44;
+console.log(age);
+console.log(typeof age)
+
+let score ="44";
+
+let valueNumber = Number(score);
+console.log(valueNumber);
+console.log(typeof valueNumber)
+
+let emptyValue = null;
+console.log(emptyValue);
+
+console.log(typeof emptyValue)
---
+++ 20JavasScriptProjects/dataTypesSummary.js
@@ -0,0 +1,6 @@
+// primtive data types
+// 7 types, string number, boolean, null, undefined, symbol, BigIn
+
+
+// Reference type: non premitive 
+// Arrany objects functions
