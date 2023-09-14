3248465fb57b35868061d6fba6cce01cfc2c9d13
Devarsh Shukla <dshukla@tritium.com.au>
2023-09-14 17:39:00 +1000

    Arrow functions learning

--- 20JavasScriptProjects/mdnJavascriptReferences/01_functions.js
+++ 20JavasScriptProjects/mdnJavascriptReferences/01_functions.js
@@ -6,10 +6,53 @@ function functionWithoutReturn(){
     console.log("This function used for processing only, no return value")
 }
 
-console.log(square()) //NaN error as no argument passed
+// console.log(square()) //NaN error as no argument passed
 
 
-console.log(square(2)) // 4
-console.log(square('d')) // invalid argument, NaN
-functionWithoutReturn()
+// console.log(square(2)) // 4
+// console.log(square('d')) // invalid argument, NaN
+// functionWithoutReturn()
+
+
+const username = 'John';
+
+function logger(){
+
+    console.log("Hello")
+    console.log("Hello")
+    console.log("Hello")
+    console.log("Hello")
+    console.log("Hello")
+}
+function upperCaseName(){
+    return username.toUpperCase();
+}
+
+// logger()
+// console.log(upperCaseName())
+
+// there are 4 ways to declare a function in JS
+// console.log(convertToLiters(2)) // this is because of hoisting this works bad practice function should be declared before calling
+// 1. Function Declaration
+function convertToLiters(gallons){
+    return gallons * 3.78541;
+}
+
+// console.log(convertToLiters(2))
+
+// 2. Function Expression
+// console.log(convertToListersExpression(2)) // this is because of hoisting
+const convertToListersExpression = function(gallons){
+    return gallons * 3.78541;
+}
+
+
+let result = convertToListersExpression(2)
+// console.log(result)
+
+// 3. Arrow Function
+// arrow functions are not hoisted
+// can not be used as con
+const convertToListersArrow = gallons => gallons * 3.78541;
+
 
