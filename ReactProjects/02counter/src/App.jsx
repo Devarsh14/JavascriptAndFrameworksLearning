819d03a78f15e1f5f49f70eabceb5c89ab336e5f
Devarsh Shukla <dshukla@tritium.com.au>
2023-12-09 14:53:04 +1000

    wip: added counter

---
+++ ReactProjects/02counter/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] =  useState(10)
+
+const addValue = () => {
+    console.log("Value added", counter);
+    setCounter(counter + 1);
+  };
+
+  return (
+    <>
+      <h1> React app</h1>
+      <h2> Counter value: {counter} </h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button>Remove Value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
