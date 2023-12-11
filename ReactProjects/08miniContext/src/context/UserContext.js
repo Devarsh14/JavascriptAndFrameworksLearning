944cc07261b09e864362b34260372ae9f5523bf3
Devarsh Shukla <dshukla@tritium.com.au>
2023-12-11 23:35:44 +1000

    Added context api

---
+++ ReactProjects/08miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//  Create a context object
+const UserContext = React.createContext();
+
+export default UserContext;
+// we have to context provider which wil be used by the components to access the data
