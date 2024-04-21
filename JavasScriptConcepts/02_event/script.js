aa211557ba8698f6c5a147add02a938e64edde3f
Devarsh Shukla <dshukla@tritium.com.au>
2024-04-21 10:54:49 +1000

    wip: added event lister

--- JavasScriptConcepts/02_event/script.js
+++ JavasScriptConcepts/02_event/script.js
@@ -1,3 +1,6 @@
+//  addEventListener is latest way to add event listener to the element
+// addEventListener is more flexible than the traditional way of adding event listener
+// addEventListener can add multiple event listener to the same element
 let div1 = document.getElementById("div1");
 div1.addEventListener("click", function () {
   console.log("Div1 is clicked");
