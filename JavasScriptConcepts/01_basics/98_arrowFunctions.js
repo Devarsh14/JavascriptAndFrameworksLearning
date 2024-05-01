d4fc4c289b8f4ed44904509cd8b2b3e2903a9e89
Devarsh Shukla <dshukla@tritium.com.au>
2024-05-01 19:52:41 +1000

    wip: to do task

---
+++ JavasScriptConcepts/01_basics/98_arrowFunctions.js
@@ -0,0 +1,15 @@
+// to export to other files or modules
+// export default function DoSomething(){
+//     console.log("DoSomething");
+// }
+// DoSomething();
+
+// // Arrow function // to export to other files or modules
+// export const DoSomething2 = () =>{
+//     return console.log("DoSomething2 from arrow function");
+// }
+// DoSomething2();
+
+
+// anonymous function
+// console.log(() => "devarsh Shukla")
---
+++ Projects/ToDoList/script.js
@@ -0,0 +1,33 @@
+document.querySelector("#push").addEventListener("click", function () {
+  console.log("Button clicked");
+  if (document.querySelector("#newtask input").value.length === 0) {
+    alert("Please enter a task");
+  } else {
+    document.querySelector("#tasks").innerHTML += ` <div class="task">
+        <span id="taskname">
+        ${document.querySelector("#newtask input").value}
+        </span>
+        <button class="delete">
+       delete
+        </button>
+        </div>`;
+
+        document.querySelector("#task").value= "";
+  }
+});
+
+
+// delete tasks
+document.addEventListener("click", function (e) {
+  if (e.target.className == "delete") {
+    e.target.parentNode.remove();
+  }
+});
+
+// complete tasks
+document.addEventListener("click", function (e) {
+  if (e.target.className == "task") {
+    e.target.classList.toggle("completed");
+  }
+});
+
